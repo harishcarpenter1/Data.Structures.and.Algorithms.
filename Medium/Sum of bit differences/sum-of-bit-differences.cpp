375aaea685404bf2b0ca9b9248ab3cb963ef6394
Harish Carpenter <92323049+harishcarpenter1@users.noreply.github.com>
2023-12-01 21:32:08 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sum of bit differences/sum-of-bit-differences.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+// 	int countDifferingBits(int a, int b) {
+//         // bitset<sizeof(int) * 8> bits(a ^ b);
+//         // integer size - 4 bytes like 2, 3, 4, 5 
+//         // total 8 * 4 = 32 bits 
+        
+//         /*  int xor_res = a^b;
+//             int count = 0;
+//             while(xor_res){
+//                 count += xor_res & 1;
+//                 xor_res >>= 1;
+//             }
+//         */
+
+//         bitset<32> bits(a ^ b);
+//         return bits.count();
+//     }
+	long long sumBitDifferences(int arr[], int n) {
+	    long long ans = 0;
+	    // 32 bits max
+	    // here arr[i] <= 10^5 so max bits log2(10^5) = 16.61
+	    // hence max 17 bits
+	    // traverse for every bit
+	    for(int i = 0; i<=17; i++){
+	        // count set bits for each number at ith position
+	        long long cntset = 0;
+	        // every number
+	        for(int j = 0; j<n; j++){
+	            // if ith bit of arr[j] is set 
+	            if(arr[j]%2==1){
+	                cntset++;
+	            }
+	            // right shift the arr[j]th number 
+	            arr[j]/=2;
+	        }
+	        // formula to count the pairs - no. of set bits * no of unset bits * 2
+	        ans += cntset*(n-cntset)*2;
+	        
+	    }
+	    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.sumBitDifferences(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends

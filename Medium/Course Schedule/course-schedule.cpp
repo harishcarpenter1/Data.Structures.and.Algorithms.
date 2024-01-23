d35fd4ef8044425ae686ace3c7ce0c870921cdf6
Harish Carpenter <92323049+harishcarpenter1@users.noreply.github.com>
2024-01-23 23:03:54 +0530

    Updated Solution - GfG to GitHub

--- Medium/Course Schedule/course-schedule.cpp
+++ Medium/Course Schedule/course-schedule.cpp
@@ -3,56 +3,51 @@
 using namespace std;
 
 // } Driver Code Ends
+//User function Template for C++
+
 
 class Solution {
-private:
-    bool dfs(vector<vector<int>>& adj, vector<int>& visited, 
-    stack<int>& st, int node, vector<int>&pathvis) {
-        visited[node] = 1;  
-        pathvis[node] = 1;
-        
-        for (int neighbor : adj[node]) {
-            if (visited[neighbor] == 0) {
-                if(dfs(adj, visited, st, neighbor, pathvis)) return true;
-            }
-            else if(pathvis[neighbor]){
-                return true;
-            }
-        }
-        
-        st.push(node);
-        pathvis[node] = 0;
-        return false;
-    }
-    
 public:
     vector<int> findOrder(int n, int m, vector<vector<int>>& p) {
         vector<vector<int>> adj(n);
         vector<int> order;
-        stack<int> st;
-        vector<int> visited(n, 0);  
-        vector<int> pathvis(n, 0);  
-        
+        queue<int> q;
+        vector<int> inDegree(n, 0);
+
         for (auto it : p) {
             adj[it[1]].push_back(it[0]);
+            inDegree[it[0]]++;
         }
-        
+
         for (int i = 0; i < n; i++) {
-            if (visited[i] == 0) {
-                if(dfs(adj, visited, st, i, pathvis)){
-                    return {};
+            if (inDegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            order.push_back(node);
+
+            for (auto neighbor : adj[node]) {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) {
+                    q.push(neighbor);
                 }
             }
         }
-        
-        while (!st.empty()) {
-            order.push_back(st.top());
-            st.pop();
+
+        if (order.size() == n) {
+            return order;
         }
-        
-        return order; 
+
+        return {};
     }
 };
+
+
+
 
 
 
